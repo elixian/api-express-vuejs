704f276994091776d9c0e50b56bb7eff35cbfb8c
Franck Colombo <franck.colombo@cnav.fr>
2020-10-05 17:49:36 +0200

    ajout d'axios test request and post

--- front-end/src/App.vue
+++ front-end/src/App.vue
@@ -1,19 +1,54 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+
+     <div v-for="data in info" :key="data._id">
+       {{data}}
+     </div>
+    <div>
+      <input type="text" v-model="email"/>
+      <input type="password" v-model="password"/>
+      <button @click="newUser">Creer utilisateur</button>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import HelloWorld from "./components/HelloWorld.vue";
+import axios from "axios";
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
+    HelloWorld,
+  },
+  data(){
+    return{
+      info:null,
+      email:null,
+      password:null
+    }
+  },
+   mounted () {
+    axios
+      .get('http://localhost:3000/api/users/')
+      .then(response => (this.info = response.data))
+  },
+  methods:{
+    newUser(){
+      axios({
+        method:'post',
+        url:'http://localhost:3000/api/users/',
+        data: {email : this.email, password : this.password}
+      })
+
+      .then(response => {
+        this.info = response.data;
+        console.log(response.error)
+        }).catch((error) => console.log(error.response.data) );
+    }
   }
-}
+};
 </script>
 
 <style>
